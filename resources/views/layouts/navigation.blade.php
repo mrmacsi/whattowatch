f80bb67b7829feb2695d5d0d3b56b07aa9333ab7
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-21 21:27:19 +0000

    change app name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('genre.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        Movie Matcher
                     </a>
                 </div>
 
