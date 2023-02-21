cc46addd4524ba393dd10c053539946ce1bc48de
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-21 22:52:12 +0000

    fix decisions title

--- resources/views/decisions.blade.php
+++ resources/views/decisions.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="bg-white">
-                        <h3 class="m-2 font-medium text-gray-900 dark:text-white">Your Genres</h3>
+                        <span class="m-2 font-medium text-gray-900 dark:text-white">Your Genres</span>
                         <span class="items-center justify-center">
                             @foreach($userGenres as $genre)
                                 <button class="m-2 px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
