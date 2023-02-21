ece2cd35780ae63baa4aec0a8062d6bdcc4957a5
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-21 23:08:00 +0000

    fix decisions title, fix dashboard space

--- resources/views/decisions.blade.php
+++ resources/views/decisions.blade.php
@@ -5,24 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div class="bg-white">
-                        <h3 class="m-2 text-lg font-medium text-gray-900">Your Genres</h3>
-                        <span class="items-center justify-center">
-                            @foreach($userGenres as $genre)
-                                <button class="m-1 px-3 py-1 border-3 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-                                    {{$genre->genre}}
-                                </button>
-                            @endforeach
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('partials.genres')
 
     <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
