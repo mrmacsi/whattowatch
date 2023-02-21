7ba5bc304cfa991a12c22b1244b7f13214145b31
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-21 17:56:43 +0000

    css fix

--- resources/views/decide.blade.php
+++ resources/views/decide.blade.php
@@ -34,7 +34,7 @@
                             <div class="mx-auto bg-white rounded-xl shadow-md overflow-hidden">
                                 <div class="md:flex">
                                     <div class="md:shrink-0 flex justify-center items-center">
-                                        <img class="object-cover w-96 rounded-lg h-full w-96 md:w-48 md:rounded-none md:rounded-lg sm:max-w-sm sm:flex" src="{{$show['quality_pic_src']}}">
+                                        <img class="object-cover w-full h-full md:w-48 md:rounded-lg sm:rounded-lg sm:max-w-sm sm:flex" src="{{$show['quality_pic_src']}}">
                                     </div>
                                     <div class="p-8">
                                         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{$show['title']}}</div>
@@ -47,11 +47,11 @@
                                         <p class="text-gray-600"><a target="_blank" href="https://www.imdb.com/title/{{$show['show_id']}}">IMDB</a></p>
                                     </div>
                                     @if(isset($show['video_src']))
-                                    <div class="md:shrink-0 flex justify-center items-center w-full">
-                                        <video class="object-cover w-full rounded-lg h-auto md:w-48 md:rounded-none md:rounded-lg sm:w-full sm:flex" autoplay muted>
-                                            <source src="{{$show['video_src']}}" type="video/mp4" />
-                                        </video>
-                                    </div>
+                                        <div class="md:shrink-0 flex justify-center items-center lg:w-96">
+                                            <video class="object-cover w-full rounded-b-lg h-auto md:w-full sm:rounded-lg md:rounded-lg sm:w-full sm:flex" autoplay muted>
+                                                <source src="{{$show['video_src']}}" type="video/mp4" />
+                                            </video>
+                                        </div>
                                     @endif
                                 </div>
                                 <div class="flex items-center justify-center mt-6">
