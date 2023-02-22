bbebe35842ceac9ceb3f3215600d72455b77f51f
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-22 15:09:54 +0000

    fix pic

--- resources/views/decide.blade.php
+++ resources/views/decide.blade.php
@@ -17,7 +17,7 @@
                             <div class="mx-auto bg-white rounded-xl shadow-md overflow-hidden">
                                 <div class="md:flex">
                                     <div class="md:shrink-0 flex justify-center items-center">
-                                        <img class="object-cover w-full h-full md:w-48 md:rounded-lg sm:rounded-lg sm:max-w-sm sm:flex" src="{{$show['quality_pic_src']}}">
+                                        <img class="object-cover w-full h-full md:w-48 md:rounded-lg sm:rounded-lg sm:max-w-sm sm:flex" src="{{$show['quality_pic_src']??$show['pic_src']}}">
                                     </div>
                                     <div class="p-4">
                                         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{$show['title']}}</div>
