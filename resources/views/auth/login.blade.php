5475de24d311f7cbaf9e6faedb2626e929a52f32
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-22 00:03:15 +0000

    register fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('login'))
-                <a class="m-4 underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                <a class="text-left p-4 justify-start items-start underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                     {{ __('Register') }}
                 </a>
             @endif
