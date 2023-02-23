7fa958ca15a16b7f109ec1766cc74d838e85b59a
Macit Simsek <macitsimsek12@hotmail.com>
2023-02-23 16:47:14 +0000

    add check on decisions

--- resources/views/decisions.blade.php
+++ resources/views/decisions.blade.php
@@ -42,7 +42,7 @@
                                         @if(auth()->user()->isAdmin())
                                             <div class="inline-flex items-center text-base font-semibold w-24 text-gray-900 dark:text-white">
                                                 <a target="_blank" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                                                   href="{{ route('show.check',['show'=>$show['id']]) }}">Check Again</a>
+                                                   href="{{ route('show.check',['show'=>$decision->show_id]) }}">Check Again</a>
                                             </div>
                                         @endif
                                         <div class="inline-flex items-center text-base font-semibold w-24 text-gray-900 dark:text-white">
